afa93cc982203b544a3e316ccaa1ad99af8af527
ilnur_ryazhapov <i.riazhapov@bars-open.ru>
2021-10-31 15:35:23 +0300

    import ormconfig to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
+import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module'
+import ormconfig from './ormconfig'
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmCoreModule.forRoot(ormconfig)],
   controllers: [AppController],
   providers: [AppService],
 })
