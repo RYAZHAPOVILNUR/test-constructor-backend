85cb936ccf06bb3e54bcb42e5a9b5bedd917e5dd
ilnur_ryazhapov <i.riazhapov@bars-open.ru>
2021-11-01 12:04:54 +0300

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
 import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module'
+import { AuthModule } from './auth/auth.module'
 import ormconfig from './ormconfig'
 
 @Module({
-  imports: [UserModule, TypeOrmCoreModule.forRoot(ormconfig)],
+  imports: [UserModule, TypeOrmCoreModule.forRoot(ormconfig), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
