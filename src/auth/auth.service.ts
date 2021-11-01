37e039ce0fa12ab1f05ce3e8b16ab4b73d568c20
ilnur_ryazhapov <i.riazhapov@bars-open.ru>
2021-11-01 12:12:38 +0300

    validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common'
+import { UserService } from '../user/user.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  public async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username)
+    if (user && user.password === password) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user
+      return result
+    }
+    return null
+  }
+}
