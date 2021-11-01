de44c347cb3e75ee25ed50cdcd8eb71cbb9913fe
ilnur_ryazhapov <i.riazhapov@bars-open.ru>
2021-11-01 14:31:15 +0300

    add ExpressRequest type

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common'
 import { LocalAuthGuard } from './local-auth.guard'
+import { ExpressRequest } from '../types/expressRequest.interface'
 
 @Controller('auth')
 export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req) {
+  async login(@Request() req: ExpressRequest) {
     return req.user
   }
 }
